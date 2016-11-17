5b926bcb527210988607edcf49499342e8a04720
Ledoux <erwan.ledoux@snips.net>
2016-11-17 16:38:51 +0100

    remove blueprints lib

--- backend/servers/flask-webrouter/app/__init__.py
+++ backend/servers/flask-webrouter/app/__init__.py
@@ -7,9 +7,6 @@ import os
 import hmac
 import json
 server_dir = '/'.join(__file__.split('/')[:-1])
-backend_dir = os.path.join(server_dir, '../../../')
-sys.path.append(os.path.join(backend_dir, 'lib'))
-from blueprints import register_blueprints
 sys.path.append(os.path.join(server_dir, 'lib'))
 sys.path.append(os.path.join(server_dir, 'modules'))
 
@@ -107,7 +104,67 @@ if is_login:
 #
 # BLUEPRINTS
 #
-register_blueprints(app, backend_dir, server_dir)
+# then we can also go to specific modules inside this webserver
+modules_path = os.path.join(server_dir, 'modules')
+if os.path.isdir(modules_path):
+    sys.path.append(modules_path)
+    module_names = map(
+        lambda fcouples:
+        fcouples[0],
+        filter(
+            lambda couples:
+            couples[-1]=='py' and couples[0] != "__init__",
+            map(
+                lambda file_name:
+                file_name.split('.'),
+                filter(
+                    lambda dir_name:
+                    dir_name[0] != ".",
+                    os.listdir(modules_path)
+                )
+            )
+        )
+    )
+else:
+    module_names = []
+# and also it specific libs
+lib_path = os.path.join(server_dir, 'lib')
+if os.path.isdir(lib_path):
+    sys.path.append(lib_path)
+    lib_names = map(
+        lambda fcouples:
+        fcouples[0],
+        filter(
+            lambda couples:
+            couples[-1]=='py' and couples[0] != "__init__",
+            map(
+                lambda file_name:
+                file_name.split('.'),
+                filter(
+                    lambda dir_name:
+                    dir_name[0] != ".",
+                    os.listdir(lib_path)
+                )
+            )
+        )
+    )
+    for lib_name in lib_names:
+        __import__(lib_name)
+# once the modules and libs are imported we can now
+# also plug the blueprints flask to the app
+map(
+    lambda module:
+    app.register_blueprint(getattr(module,'blueprint')),
+    filter(
+        lambda module:
+        hasattr(module,'blueprint'),
+        map(
+            lambda module_name:
+            __import__(module_name),
+            module_names
+        )
+    )
+)
 
 #
 # TABLES
