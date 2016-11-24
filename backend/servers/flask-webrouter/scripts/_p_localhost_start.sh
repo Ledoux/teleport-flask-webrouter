5cffa110a31a521b2150f128e7baf11decd97d63
Ledoux <erwan.ledoux@snips.net>
2016-11-24 15:05:51 +0100

    Removed venv dep

--- backend/servers/flask-webrouter/scripts/_p_localhost_start.sh
+++ backend/servers/flask-webrouter/scripts/_p_localhost_start.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-if [ -d "../../../$(project.config.venv)" ] ; then
-  source ../../../$(project.config.venv)/bin/activate
+if ["$VIRTUAL_ENV" != ""] ; then
+  $VIRTUAL_ENV_OPTION = -H $VIRTUAL_ENV
+else
+  $VIRTUAL_ENV_OPTION = ""
 fi
-if [ "$MODE" != "localhost" ] ; then
-  $(manageExtraConfig) uwsgi -H ../../../$(project.config.venv) --ini config/localhost_uwsgi.ini
+if [ "$MODE" != "localhost"] ; then
+  $(manageExtraConfig) uwsgi $VIRTUAL_ENV_OPTION --ini config/localhost_uwsgi.ini
 else
   $(manageExtraConfig) python scripts/manage.py runserver
 fi
