27ebe58e978616bd32b936fd752cd01d1393cf14
Ledoux <erwan.ledoux@snips.net>
2016-11-30 10:58:42 +0100

    fixed localhost start

--- backend/servers/flask-webrouter/scripts/_p_localhost_start.sh
+++ backend/servers/flask-webrouter/scripts/_p_localhost_start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-if ["$VIRTUAL_ENV" != ""] ; then
-  $VIRTUAL_ENV_OPTION = -H $VIRTUAL_ENV
+if [ "$VIRTUAL_ENV" != "" ] ; then
+  VIRTUAL_ENV_OPTION = -H $VIRTUAL_ENV
 else
-  $VIRTUAL_ENV_OPTION = ""
+  VIRTUAL_ENV_OPTION = ""
 fi
-if [ "$MODE" != "localhost"] ; then
+if [ "$MODE" != "localhost" ] ; then
   $(manageExtraConfig) uwsgi $VIRTUAL_ENV_OPTION --ini config/localhost_uwsgi.ini
 else
   $(manageExtraConfig) python scripts/manage.py runserver
