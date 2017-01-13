5e705d180275744cdca75089eb902c60c7f53bc2
Ledoux <erwan.ledoux@snips.net>
2017-01-13 16:58:24 +0100

    cleaned the template with heroku

--- backend/servers/flask-webrouter/scripts/_p_heroku_build.sh
+++ backend/servers/flask-webrouter/scripts/_p_heroku_build.sh
@@ -1,3 +1,7 @@
-if [[ ! $(heroku apps:info -a $(run.tag)) ]]; then
-  git init && heroku create --app $(run.tag)
+if [[ ! $(heroku apps:info -a $[run.tag]) ]]; then
+  git init
+  heroku create --app $[run.tag] --buildpack heroku/python --remote $[type.name]
+  heroku config:set --app $[run.tag] TYPE=$[type.name]
+else
+  echo "$[run.tag] has been already created"
 fi
