8f516681ce1ae52a84ec2d6174647e678b9302e6
erwanledoux <lawanledoux@gmail.com>
2017-02-01 17:06:45 +0100

    fixed typo in VIRTUAL_ENV_OPTION

--- backend/servers/flask-webrouter/scripts/_p_localhost_start.sh
+++ backend/servers/flask-webrouter/scripts/_p_localhost_start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ "$VIRTUAL_ENV" != "" ] ; then
-  VIRTUAL_ENV_OPTION = -H $VIRTUAL_ENV
+  VIRTUAL_ENV_OPTION="-H $VIRTUAL_ENV"
 else
-  VIRTUAL_ENV_OPTION = ""
+  VIRTUAL_ENV_OPTION=""
 fi
 if [ "$MODE" != "localhost" ] ; then
   $[manageExtraConfig] export URL=$[run.url] && uwsgi $VIRTUAL_ENV_OPTION --ini config/localhost_uwsgi.ini
